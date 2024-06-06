dff8916ea7eca23a2e288045dde9010ed56b3297
Heemin-Ju <5degreet@gmail.com>
2024-06-06 20:06:48 +0900

    README.md and firebase hosting

---
+++ public/js/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA8hKyRIZ80WStr30ayRGZ-YIGBbwP7Bms",
+  authDomain: "rarity-collection-98cd2.firebaseapp.com",
+  projectId: "rarity-collection-98cd2",
+  storageBucket: "rarity-collection-98cd2.appspot.com",
+  messagingSenderId: "1009564243558",
+  appId: "1:1009564243558:web:8b083cfab057efe50b7fa7",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
